cd7fbb95b8db325c3042954b9246a3e17bf30fd4
FlameFlag <github@flameflag.dev>
2026-04-16 17:49:21 +0300

    opencode: fix `node_modules` hash override

--- pkgs/by-name/op/opencode/package.nix
+++ pkgs/by-name/op/opencode/package.nix
@@ -1,10 +1,17 @@
 { opencode, fetchFromGitHub }:
-opencode.overrideAttrs (oldAttrs: {
-  version = "github-v1.2.22-unstable-2026-04-16";
-  src = fetchFromGitHub {
-    inherit (oldAttrs.src) owner repo;
+let
+  newSrc = fetchFromGitHub {
+    inherit (opencode.src) owner repo;
     tag = "v1.4.3";
     hash = "sha256-m+Ue7FWiTjKMAn1QefAwOMfOb2Vybk0mJPV9zcbkOmE=";
   };
-  npmDepsHash = "sha256-hVXlQcUuvUudIB35Td6ucBYopM/QOSx59tQbCTqoB/0=";
+in
+opencode.overrideAttrs (oldAttrs: {
+  version = "github-v1.2.22-unstable-2026-04-16";
+  src = newSrc;
+  node_modules = oldAttrs.node_modules.overrideAttrs {
+    inherit (oldAttrs) version;
+    src = newSrc;
+    outputHash = "sha256-hVXlQcUuvUudIB35Td6ucBYopM/QOSx59tQbCTqoB/0=";
+  };
 })
