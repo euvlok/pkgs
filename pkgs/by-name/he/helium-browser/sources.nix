1b9ad24f812719c555a5d986bef20e17de96ccfe
FlameFlag <git@ps1.sh>
2026-05-01 12:11:16 +0300

    helium-browser: bump

--- pkgs/by-name/he/helium-browser/sources.nix
+++ pkgs/by-name/he/helium-browser/sources.nix
@@ -1,24 +1,24 @@
 { fetchurl }:
 {
   aarch64-darwin = {
-    version = "0.11.5.1";
+    version = "0.11.7.1";
     src = fetchurl {
-      url = "https://github.com/imputnet/helium-macos/releases/download/0.11.5.1/helium_0.11.5.1_arm64-macos.dmg";
-      hash = "sha256-P5iXtXS05uu5Qy9jPheXAbjewn6jKTcqc5uF2yZoz/k=";
+      url = "https://github.com/imputnet/helium-macos/releases/download/0.11.7.1/helium_0.11.7.1_arm64-macos.dmg";
+      hash = "sha256-jbu/w3/GYC60Gs0ueeECrbeDDSLAoXFManquIpw0VvM=";
     };
   };
   aarch64-linux = {
-    version = "0.11.5.1";
+    version = "0.11.7.1";
     src = fetchurl {
-      url = "https://github.com/imputnet/helium-linux/releases/download/0.11.5.1/helium-0.11.5.1-arm64.AppImage";
-      hash = "sha256-f3nTqFVlgOIObtvtA41w17zcguaxjc54I59anCPoM38=";
+      url = "https://github.com/imputnet/helium-linux/releases/download/0.11.7.1/helium-0.11.7.1-arm64.AppImage";
+      hash = "sha256-OfI7oDveklNmKef9u8RHInbKT+jldaXMDgs8BSdiaUs=";
     };
   };
   x86_64-linux = {
-    version = "0.11.5.1";
+    version = "0.11.7.1";
     src = fetchurl {
-      url = "https://github.com/imputnet/helium-linux/releases/download/0.11.5.1/helium-0.11.5.1-x86_64.AppImage";
-      hash = "sha256-Ni7IZ9UBafr+ss0BcQaRKqmlmJI4IV1jRAJ8jhcodlg=";
+      url = "https://github.com/imputnet/helium-linux/releases/download/0.11.7.1/helium-0.11.7.1-x86_64.AppImage";
+      hash = "sha256-qzc135IP5F2btxtOMUGMz+0azJhYL9KI0lcPG2KjcxU=";
     };
   };
 }
