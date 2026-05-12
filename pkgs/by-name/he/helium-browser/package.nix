626b12759141b09a4f331ed21a138306aa583bca
FlameFlag <git@ps1.sh>
2026-05-12 13:40:46 +0300

    helium-browser: fix for non-Gnome users... smh...

--- pkgs/by-name/he/helium-browser/package.nix
+++ pkgs/by-name/he/helium-browser/package.nix
@@ -5,23 +5,47 @@
   _7zz,
   makeWrapper,
   autoPatchelfHook ? null,
-  gtk3 ? null,
+  alsa-lib ? null,
+  at-spi2-atk ? null,
+  at-spi2-core ? null,
   atk ? null,
   cairo ? null,
-  pango ? null,
-  atSpi2Atk ? null,
-  atSpi2Core ? null,
-  nss ? null,
-  nspr ? null,
+  cups ? null,
   dbus ? null,
   expat ? null,
-  cups ? null,
-  alsaLib ? null,
-  xorg ? null,
-  libxkbcommon ? null,
-  libgbm ? null,
+  fontconfig ? null,
+  freetype ? null,
+  gcc-unwrapped ? null,
+  gdk-pixbuf ? null,
+  glib ? null,
+  gtk3 ? null,
+  gtk4 ? null,
+  libdrm ? null,
   libglvnd ? null,
+  libX11 ? null,
+  libxcb ? null,
+  libXcomposite ? null,
+  libXcursor ? null,
+  libXdamage ? null,
+  libXext ? null,
+  libXfixes ? null,
+  libXi ? null,
+  libxkbcommon ? null,
+  libXrandr ? null,
+  libXrender ? null,
+  libXScrnSaver ? null,
+  libxshmfence ? null,
+  libXtst ? null,
+  libgbm ? null,
+  nspr ? null,
+  nss ? null,
+  pango ? null,
+  pipewire ? null,
+  qt5 ? null,
+  qt6 ? null,
   udev ? null,
+  vulkan-loader ? null,
+  wayland ? null,
   glibc ? null,
 
   # command line arguments which are always set e.g. "--disable-gpu"
@@ -107,29 +131,48 @@ else
     ];
     buildInputs = lib.optionals stdenvNoCC.hostPlatform.isLinux [
       glibc
-      gtk3
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
       atk
       cairo
-      pango
-      atSpi2Atk
-      atSpi2Core
-      nss
-      nspr
+      cups
       dbus
       expat
-      cups
-      alsaLib
-      xorg.libX11
-      xorg.libxcb
-      xorg.libXcomposite
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXfixes
-      xorg.libXrandr
-      libxkbcommon
-      libgbm
+      fontconfig
+      freetype
+      gcc-unwrapped.lib
+      gdk-pixbuf
+      glib
+      gtk3
+      gtk4
+      libdrm
       libglvnd
+      libX11
+      libxcb
+      libXcomposite
+      libXcursor
+      libXdamage
+      libXext
+      libXfixes
+      libXi
+      libxkbcommon
+      libXrandr
+      libXrender
+      libXScrnSaver
+      libxshmfence
+      libXtst
+      libgbm
+      nspr
+      nss
+      pango
+      pipewire
+      qt5.qtbase
+      qt6.qtbase
+      qt6.qtwayland
       udev
+      vulkan-loader
+      wayland
     ];
 
     installPhase = ''
