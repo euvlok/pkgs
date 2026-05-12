3668656ba15d87e8ed2a61c4266660e31c9c8f49
FlameFlag <git@ps1.sh>
2026-05-12 14:02:27 +0300

    helium-browser: fix loonix on non-gnome

--- pkgs/by-name/he/helium-browser/package.nix
+++ pkgs/by-name/he/helium-browser/package.nix
@@ -5,6 +5,14 @@
   _7zz,
   makeWrapper,
   autoPatchelfHook ? null,
+  addDriverRunpath ? null,
+  coreutils ? null,
+  xdg-utils ? null,
+  makeFontsConf ? null,
+  noto-fonts-cjk-sans ? null,
+  noto-fonts-cjk-serif ? null,
+  adwaita-icon-theme ? null,
+  gsettings-desktop-schemas ? null,
   alsa-lib ? null,
   at-spi2-atk ? null,
   at-spi2-core ? null,
@@ -20,6 +28,7 @@
   glib ? null,
   gtk3 ? null,
   gtk4 ? null,
+  ffmpeg ? null,
   libdrm ? null,
   libglvnd ? null,
   libX11 ? null,
@@ -30,6 +39,10 @@
   libXext ? null,
   libXfixes ? null,
   libXi ? null,
+  libICE ? null,
+  libSM ? null,
+  libXt ? null,
+  libXft ? null,
   libxkbcommon ? null,
   libXrandr ? null,
   libXrender ? null,
@@ -37,15 +50,20 @@
   libxshmfence ? null,
   libXtst ? null,
   libgbm ? null,
+  libkrb5 ? null,
+  libuuid ? null,
+  libva ? null,
+  libxml2 ? null,
   nspr ? null,
   nss ? null,
   pango ? null,
   pipewire ? null,
-  qt5 ? null,
   qt6 ? null,
+  snappy ? null,
   udev ? null,
   vulkan-loader ? null,
   wayland ? null,
+  zlib ? null,
   glibc ? null,
 
   # command line arguments which are always set e.g. "--disable-gpu"
@@ -58,6 +76,69 @@ let
 
   pname = "helium-browser";
   inherit (sources.${system}) version src;
+
+  linuxDeps = [
+    glibc
+    alsa-lib
+    at-spi2-atk
+    at-spi2-core
+    atk
+    cairo
+    cups
+    dbus
+    expat
+    ffmpeg
+    fontconfig
+    freetype
+    gcc-unwrapped.lib
+    gdk-pixbuf
+    glib
+    gtk3
+    gtk4
+    libdrm
+    libglvnd
+    libICE
+    libkrb5
+    libSM
+    libuuid
+    libva
+    libX11
+    libxcb
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXft
+    libXi
+    libxkbcommon
+    libXrandr
+    libXrender
+    libXScrnSaver
+    libxshmfence
+    libXt
+    libXtst
+    libgbm
+    libxml2
+    nspr
+    nss
+    pango
+    pipewire
+    qt6.qtbase
+    qt6.qtwayland
+    snappy
+    udev
+    vulkan-loader
+    wayland
+    zlib
+  ];
+
+  fontsConf = makeFontsConf {
+    fontDirectories = [
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+    ];
+  };
 
   meta = {
     description = "Private, fast, and honest web browser based on ungoogled-chromium";
@@ -124,65 +205,48 @@ else
     dontConfigure = true;
     dontBuild = true;
     dontStrip = true;
+    dontWrapQtApps = true;
 
     nativeBuildInputs = lib.optionals stdenvNoCC.hostPlatform.isLinux [
       autoPatchelfHook
       makeWrapper
+      qt6.wrapQtAppsHook
     ];
-    buildInputs = lib.optionals stdenvNoCC.hostPlatform.isLinux [
-      glibc
-      alsa-lib
-      at-spi2-atk
-      at-spi2-core
-      atk
-      cairo
-      cups
-      dbus
-      expat
-      fontconfig
-      freetype
-      gcc-unwrapped.lib
-      gdk-pixbuf
-      glib
-      gtk3
-      gtk4
-      libdrm
-      libglvnd
-      libX11
-      libxcb
-      libXcomposite
-      libXcursor
-      libXdamage
-      libXext
-      libXfixes
-      libXi
-      libxkbcommon
-      libXrandr
-      libXrender
-      libXScrnSaver
-      libxshmfence
-      libXtst
-      libgbm
-      nspr
-      nss
-      pango
-      pipewire
-      qt5.qtbase
-      qt6.qtbase
-      qt6.qtwayland
-      udev
-      vulkan-loader
-      wayland
-    ];
+    buildInputs = lib.optionals stdenvNoCC.hostPlatform.isLinux (
+      linuxDeps
+      ++ [
+        adwaita-icon-theme
+        gsettings-desktop-schemas
+      ]
+    );
 
     installPhase = ''
       runHook preInstall
 
-      mkdir -p "$out/libexec/helium" "$out/bin"
+      mkdir -p "$out/libexec/helium" "$out/bin" "$out/share/applications" "$out/share/icons/hicolor/256x256/apps"
       tar -xJf "$src" -C "$out/libexec/helium" --strip-components=1
+      rm -f "$out/libexec/helium/libqt5_shim.so"
+      install -Dm644 "$out/libexec/helium/helium.desktop" "$out/share/applications/helium-browser.desktop"
+      install -Dm644 "$out/libexec/helium/product_logo_256.png" "$out/share/icons/hicolor/256x256/apps/helium.png"
+      substituteInPlace "$out/share/applications/helium-browser.desktop" \
+        --replace-fail "Exec=helium %U" "Exec=$out/bin/helium-browser %U" \
+        --replace-fail "Exec=helium --incognito" "Exec=$out/bin/helium-browser --incognito"
+      sed -i "s|^Exec=helium$|Exec=$out/bin/helium-browser|" "$out/share/applications/helium-browser.desktop"
 
       makeWrapper "$out/libexec/helium/helium-wrapper" "$out/bin/helium-browser" \
-        --add-flags ${lib.escapeShellArg commandLineArgs}
+        --add-flags ${lib.escapeShellArg commandLineArgs} \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto}}" \
+        --set-default CHROME_VERSION_EXTRA nix \
+        --set FONTCONFIG_FILE "${fontsConf}" \
+        --prefix XDG_DATA_DIRS : "${addDriverRunpath.driverLink}/share:${gtk3}/share/gsettings-schemas/${gtk3.name}:${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${adwaita-icon-theme}/share" \
+        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath linuxDeps} \
+        --prefix PATH : ${
+          lib.makeBinPath [
+            coreutils
+            xdg-utils
+          ]
+        } \
+        ''${qtWrapperArgs[@]}
       ln -s helium-browser "$out/bin/helium"
 
       runHook postInstall
